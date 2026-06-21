2e44a0aa4faf6992d9e727291f4bd89d58622332
Samuel de Almeida <samuel.silva411@outlook.com>
2026-06-21 17:40:03 -0300

    criar a estrutura do projeto

---
+++ contacts_registration/src/test/java/br/com/samucadev/contacts_registration/ContactsRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samucadev.contacts_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
